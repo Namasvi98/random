6791bed07bc02a17137e615877ec972c2acf0137
Namasvi98 <75670277+Namasvi98@users.noreply.github.com>
2022-02-15 15:57:35 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage("Bed.jpeg");
+}
+
+function setup()
+{
+    Canvas = createCanvas(640, 500);
+    Canvas.center();
+
+    COCOSSD = ml5.objectDetector("cocossd", modelLoadedDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD);
+}
+
+function modelLoadedDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD()
+{
+    console.log("Model has Loadeddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd pls check if not loadedddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+    status = true;
+    COCOSSD.detect(img, gotresultsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss);
+    document.getElementById("status").innerHTML = "Object detecting";  
+}
+
+function gotresultsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 500);
+    fill("red");
+    text("Bed", 25, 25);
+    noFill();
+    stroke("#FF0000");
+    rect(10, 10, 610, 490);
+}
